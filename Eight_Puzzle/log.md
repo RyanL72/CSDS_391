7b3bc492acb65445ed90cde15c630d868d1c65b0
Ryan_Lin2 <rhl72@case.edu>
2024-09-02 00:41:40 -0400

    In need of Sphinx Documentation

--- Eight_Puzzle/log.md
+++ Eight_Puzzle/log.md
@@ -7,6 +7,19 @@ pip freeze > requirements.txt
 
 pip install sphinx
 sphinx-quickstart
-.\make.bat html
-### this one works globally
-sphinx-build -b html source/ build/html/ 
+
+go to config.py and import os, import sys, and sys.path.insert(0, os.path.abspath(r'C:\Users\Ryan\CSDS_391\Eight_Puzzle\src'))
+
+go to index.rst file and add modules under Contents
+
+in documentation directory  
+sphinx-apidoc -o . ..
+./make.bat html
+
+------
+
+sphinx-apidoc -o C:\Users\Ryan\CSDS_391\Eight_Puzzle\documentation\source 
+C:\Users\Ryan\CSDS_391\Eight_Puzzle\documentation\make.bat html
+
+pip install sphinx-autobuild
+sphinx-autobuild . ..
--- Eight_Puzzle/src/Eight_Puzzle.py
+++ Eight_Puzzle/src/Eight_Puzzle.py
@@ -1,24 +1,18 @@
 class Eight_Puzzle:
-    """
-    A class that represents an 8 puzzle
-
-    Attributes
-    ----------
-    __puzzleConfiguration: List[int]
-
-    Methods
-    -------
-    """
+    
     def __init__(self, puzzleConfiguration):
+        
         self.__puzzleConfiguration = puzzleConfiguration
     
     def printPuzzleConfiguration(self):
+        
         size = int(len(self.__puzzleConfiguration) ** 0.5)
         for i in range(size):
             row = self.__puzzleConfiguration[i * size:(i + 1) * size]
             print(" ".join(map(str, row)))
     
 if __name__ == "__main__":
-    array = [0,1,2,3,4,5,6,7,8]
+    array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     newEightPuzzle = Eight_Puzzle(array)
     newEightPuzzle.printPuzzleConfiguration()
+
