d52b4bc3cd4530bd26f77fb45f437d6f7b46a551
Ryan_Lin2 <rhl72@case.edu>
2024-09-03 19:20:30 -0400

    Closer

--- src/Eight_Puzzle/Eight_Puzzle.py
+++ src/Eight_Puzzle/Eight_Puzzle.py
@@ -2,6 +2,21 @@ class Eight_Puzzle:
     
     def __init__(self, puzzleConfiguration=[[0,1,2],[3,4,5],[6,7,8]]):
         self.__puzzleConfiguration = puzzleConfiguration
+        '''
+        puzzleConfiguration[y][x]
+
+        .------------------------> (X)
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        v
+        (Y)
+
+        '''
         self.__x = None
         self.__y = None
         self.updateZeroPosition()
@@ -11,7 +26,7 @@ class Eight_Puzzle:
     def setState(self, newPuzzleConfiguration):
         self.__puzzleConfiguration = newPuzzleConfiguration
         self.updateZeroPosition()
-        print(f"Set Zero to {self.__x} {self.__y}")
+        #print(f"Set Zero to {self.__x} {self.__y}")
         
         
 
@@ -23,50 +38,71 @@ class Eight_Puzzle:
    
     def move(self, direction):
         validMoves = self.getValidMoves()
-        print(f"The valid moves are: {validMoves}")
-        print(f"Before this move, the current zero is at {self.__x} {self.__y}")
+        #print(f"The valid moves are: {validMoves}")
+        #print(f"Before this move, the current zero is at {self.__x} {self.__y}")
         if direction in validMoves:
                 
                 currentX = self.__x
                 currentY = self.__y
 
                 if direction == "Right":
-                    print("Moving Right")
+                    #print("Moving Right")
                     swapPosition = currentX + 1
+                    #print(f" -the position we are swapping is swapPosition {swapPosition} right")
                     newState = [row[:] for row in self.__puzzleConfiguration] 
+                    #print(f" -copy of old array: {newState}")
                     swapPtr = newState[currentY][swapPosition]
+                    #print(f" -The value we need to swap into blank: {swapPtr}")
                     newState[currentY][swapPosition] = newState[currentY][currentX]
+                    #print(f" -The new configuration step 1: {newState}")
                     newState[currentY][currentX] = swapPtr
+                    #print(f" -The new configuration step 2: {newState}")
                     self.setState(newState)
                     return
 
                 elif direction == "Down":
-                    print("Moving Down")
+                    #print("Moving Down")
                     swapPosition = currentY + 1
+                    #print(f" -the position we are swapping is swapPosition {swapPosition} down")
                     newState = [row[:] for row in self.__puzzleConfiguration] 
+                    #print(f" -copy of old array: {newState}")
                     swapPtr = newState[swapPosition][currentX]
+                    #print(f" -The value we need to swap into blank: {swapPtr}")
                     newState[swapPosition][currentX] = newState[currentY][currentX]
+                    #print(f" -The new configuration step 1: {newState}")
                     newState[currentY][currentX] = swapPtr
+                    #print(f" -The new configuration step 2: {newState}")
                     self.setState(newState)
                     return
 
                 elif direction == "Left":
-                    print("Moving Left")
+                    #print("Moving Left")
                     swapPosition = currentX - 1
+                    #print(f" -the position we are swapping is swapPosition {swapPosition} left")
                     newState = [row[:] for row in self.__puzzleConfiguration]  
+                    #print(f" -copy of old array: {newState}")
                     swapPtr = newState[currentY][swapPosition]
+                    #print(f" -The value we need to swap into blank: {swapPtr}")
                     newState[currentY][swapPosition] = newState[currentY][currentX]
+                    #print(f" -The new configuration step 1: {newState}")
                     newState[currentY][currentX] = swapPtr
+                    #print(f" -The new configuration step 2: {newState}")
                     self.setState(newState)
                     return
 
                 elif direction == "Up":
-                    print("Moving Up")
+                    #print("Moving Up")
+                    #print(f" -the currentY: {currentY}")
                     swapPosition = currentY - 1
+                    #print(f" -the position we are swapping is swapPosition {swapPosition} up")
                     newState = [row[:] for row in self.__puzzleConfiguration]  
+                    #print(f" -copy of old array: {newState}")
                     swapPtr = newState[swapPosition][currentX]
+                    #print(f" -The value we need to swap into blank: {swapPtr}")
                     newState[swapPosition][currentX] = newState[currentY][currentX]
+                    #print(f" -The new configuration step 1: {newState}")
                     newState[currentY][currentX] = swapPtr
+                    #print(f" -The new configuration step 2: {newState}")
                     self.setState(newState)
                     return
 
@@ -75,7 +111,7 @@ class Eight_Puzzle:
 
     def getValidMoves(self):
         position = self.getZeroPosition()
-        print(f"Our Zero Position is {position}")
+        #print(f"Our Zero Position is {position}")
         if position == (0, 0):
             return ["Right", "Down"]
         elif position == (1, 0):
@@ -101,9 +137,8 @@ class Eight_Puzzle:
         for i, row in enumerate(self.__puzzleConfiguration):
             for j, element in enumerate(row):
                 if element == 0:
-                    self.__x = i
-                    self.__y = j
-                    print(f"The new Zero Position is {self.getZeroPosition()}")
+                    self.__x = j
+                    self.__y = i
                     return
         raise ValueError("No Zero Value")
 
@@ -118,24 +153,42 @@ class Eight_Puzzle:
 if __name__ == "__main__":
 
     newEightPuzzle = Eight_Puzzle()
+
+
+    newEightPuzzle.setState([[0,1,2],[3,4,5],[6,7,8]])
     newEightPuzzle.printState()
-
-
-    newEightPuzzle.setState([[8,7,6], [5,4,3],[2,1,0]])
-    newEightPuzzle.printState()
-
+    print(newEightPuzzle.getZeroPosition())
 
     print("---------")
-
-    newEightPuzzle.setState([[1,2,3],[4,0,5],[6,7,8]])
-    newEightPuzzle.printState()
-
     newEightPuzzle.move("Right")
     newEightPuzzle.printState()
+    print(newEightPuzzle.getZeroPosition())
+    print("---------")
 
-    newEightPuzzle.move("Up")
+    newEightPuzzle.move("Down")
     newEightPuzzle.printState()
+    print(newEightPuzzle.getZeroPosition())
+    print("---------")
 
+    newEightPuzzle.move("Left")
+    newEightPuzzle.printState()
+    print(newEightPuzzle.getZeroPosition())
+    print("---------")
+
+    newEightPuzzle.move("Down")
+    newEightPuzzle.printState()
+    print(newEightPuzzle.getZeroPosition())
+    print("---------")
+
+    # newEightPuzzle.move("Up")
+    newEightPuzzle.printState()
+    print(newEightPuzzle.getZeroPosition())
+    print("---------")
+
+    # newEightPuzzle.move("Up")
+    newEightPuzzle.printState()
+    print(newEightPuzzle.getZeroPosition())
+    print("---------")
 
 
 
