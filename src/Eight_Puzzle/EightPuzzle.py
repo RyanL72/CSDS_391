ac493b1613ce5f7baa32ba897fb1a6bcbea4b6e9
Ryan_Lin2 <94211083+Ryanlin2@users.noreply.github.com>
2024-09-23 15:52:01 -0400

    storeState was replaced with state history

--- src/Eight_Puzzle/EightPuzzle.py
+++ src/Eight_Puzzle/EightPuzzle.py
@@ -7,7 +7,6 @@ class EightPuzzle:
     def __init__(self, puzzleConfiguration=[[0,1,2],[3,4,5],[6,7,8]]):
         self.__puzzleConfiguration = puzzleConfiguration
         self.__stateHistory = set()
-        self.storeState()
         '''
         Assignment 1
 
